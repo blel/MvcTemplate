7567fc3d65e5f41506c1c1f110b5cad233a7050f
Elias Balmer <elias.balmer@bluewin.ch>
2013-01-29 06:34:45 +0100

    commets added

---
+++ MvcTempates/Models/EmployerSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTempates.Models
+{
+    public class EmployerSearchViewModel
+    {
+        public IEnumerable<Employer> ResultSet { get; set; }
+        public string SearchText { get; set; }
+        public int MaxSearchResult { get; set; }
+        public int RequestedPage { get; set; }
+        public HtmlHelperExtensions.TableActions TableAction { get; set; }
+
+
+    }
+}
